ca3c41de4dd0be52771482c1cf9e6586ba286639
2ewqasd <specforworkwithpeop@gmail.com>
2020-09-24 21:37:13 +0300

    Edit viwes.py Rfit searilizer.py Create templates Create swagger-ui.html src ./lab1/templates/ Edit swagger-ui.html Edit urls.py - add OpenApi schema and router

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from person import views
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -10,5 +12,21 @@ router.register(r'person', views.UserViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+        # Use the `get_schema_view()` helper to add a `SchemaView` to project URLs.
+    #   * `title` and `description` parameters are passed to `SchemaGenerator`.
+    #   * Provide view name for use with `reverse()`.
+    path('openapi', get_schema_view(
+        title="Lab1",
+        description="Try to learn",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    
+    # Route TemplateView to serve Swagger UI template.
+    #   * Provide `extra_context` with view name of `SchemaView`.
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
--- person/serializers.py
+++ person/serializers.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Group
 from rest_framework import serializers
+from .models import Person
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = User
+        model = Person
         fields = ['url', 'first_name', 'second_name', 'years_old','mobile_number']
 
 
